5e9e657bc9050d5546d0327ee66159e00b7616a8
Javier Sánchez Rois <javier.sanchez.rois@gmail.com>
2014-12-03 17:37:10 +0000

    Parsing functions to hash

--- sample.rb
+++ sample.rb
@@ -1,12 +1,21 @@
 class ClassNode
- attr_accessor :type,:name,:body,:functs
+ attr_accessor :type,:name,:functs
+ attr_writer :body
+ 
+ def parse_body(body)
+  functs = Hash.new(0)
+  body.scan(/([a-zA-Z])+\s+([^\(\s]+)\s*\(([^\)]*)\)/m).each do |m|
+    #          ^ retval        ^ name         ^ params 
+    functs[m[1].to_sym] = {:retval => m[0], :parameters => m[2]}
+  end
+ end 
+ 
  def initialize(data)
   @type = data[:type]
   @name = data[:name]
   @body = data[:body]
   #parse functions!
-  @functs = body.scan(/[a-zA-Z]+\s+([^\(\s]+)\s*\(([^\)]*)\)/m)
-  
+  parse_body(@body)
  end 
  def inspect
   "#{@type} #{@name} contains \n#{@body} \n with functions #{@functs}"
